4ff300a87d7cba98924b192265dfb905f5050a48
oksk1111 <oksk1111@gmail.com>
2022-10-23 23:45:24 +0900

    Initial commit

---
+++ src/main/java/com/example/reactjs_board/ReactjsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactjs_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactjsBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactjs_board/ReactjsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactjs_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactjsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
